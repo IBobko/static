7a17d81a63fa566d9200503f26425c084ac4b887
Igor Bobko <limit-speed@yandex.ru>
2016-09-03 15:36:24 +0300

    Added removing files

--- src/main/java/ru/todo100/files/controller/RemoveController.java
+++ src/main/java/ru/todo100/files/controller/RemoveController.java
@@ -15,7 +15,7 @@ import java.nio.file.*;
 @RequestMapping("/remove/file")
 public class RemoveController {
     @ResponseBody
-    @RequestMapping("/{filename}")
+    @RequestMapping("/{filename:.+}")
     public String index(@PathVariable String filename){
         Path path = Paths.get("C:/Server/" + filename);
         try {
